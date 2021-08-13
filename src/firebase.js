16c759dc897f2d37545f90ef0f227da95dc3195f
Debadrita Ghosh <62397316+DebadritaGhosh@users.noreply.github.com>
2021-08-13 12:18:55 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDYncrs2z-hHA1GNkAEOeU7gpEZYkPFt28",
-    authDomain: "todo-app-f6c40.firebaseapp.com",
-    projectId: "todo-app-f6c40",
-    storageBucket: "todo-app-f6c40.appspot.com",
-    messagingSenderId: "642455150973",
-    appId: "1:642455150973:web:bf4d7ef9f26b69714dec14",
-    measurementId: "G-PFKLKH5PR3"
+    //Your Firebase credentials
 });
 
 const db = firebaseApp.firestore();
